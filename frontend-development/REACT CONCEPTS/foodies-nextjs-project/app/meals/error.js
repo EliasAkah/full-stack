ee979a98fb8567f6404e7773c30bd751612cb99e
David Akah <eliasakah1999@gmail.com>
2025-06-18 20:08:40 +0530

    Use Nextjs to build fullstack application

---
+++ frontend-development/REACT CONCEPTS/foodies-nextjs-project/app/meals/error.js
@@ -0,0 +1,9 @@
+"use client";
+export default function Error() {
+  return (
+    <header className="error">
+      <h1>An Error Occured</h1>
+      <p>Unable to Fetch Meals, Please Try Again Later</p>
+    </header>
+  );
+}
---
+++ frontend-development/REACT CONCEPTS/foodies-nextjs-project/app/meals/share/page.js
@@ -0,0 +1,7 @@
+export default function SharedMeal() {
+  return (
+    <main>
+      <h1>This is the meal that was shared by You</h1>
+    </main>
+  );
+}
