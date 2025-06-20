752bc422facc8228db4412fd9a67faaac4535c2f
David Akah <eliasakah1999@gmail.com>
2025-06-20 19:42:27 +0530

    fix mongodb connection by calling MongoClient within each function, revalidate each static page, install eslint and create a configuration file for it

--- frontend-development/REACT CONCEPTS/react-meetup-project-page-route/.next/static/webpack/webpack.27a9b81dc39e1e8d.hot-update.js
+++ frontend-development/REACT CONCEPTS/react-meetup-project-page-route/.next/static/webpack/webpack.27a9b81dc39e1e8d.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ (() => {
-/******/ 	__webpack_require__.h = () => ("a6af472548d940e2")
+/******/ 	__webpack_require__.h = () => ("b87152c78aa55fc4")
 /******/ })();
 /******/ 
 /******/ }
--- frontend-development/REACT CONCEPTS/react-meetup-project-page-route/.next/static/webpack/webpack.b87152c78aa55fc4.hot-update.js
+++ frontend-development/REACT CONCEPTS/react-meetup-project-page-route/.next/static/webpack/webpack.b87152c78aa55fc4.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ (() => {
-/******/ 	__webpack_require__.h = () => ("e8898ae1c1e85d21")
+/******/ 	__webpack_require__.h = () => ("7fd5afa4d908459e")
 /******/ })();
 /******/ 
 /******/ }
