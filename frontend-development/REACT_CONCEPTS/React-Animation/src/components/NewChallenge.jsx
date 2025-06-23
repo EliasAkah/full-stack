5ee79193fc9bde98b5f3a6579a46fdfedf87c533
David Akah <eliasakah1999@gmail.com>
2025-06-23 12:37:55 +0530

    fix issue with variants, learn more features of framer motion

--- frontend-development/REACT_CONCEPTS/React-Animation/src/components/NewChallenge.jsx
+++ frontend-development/REACT_CONCEPTS/React-Animation/src/components/NewChallenge.jsx
@@ -1,13 +1,15 @@
-import { useContext, useRef, useState } from 'react';
+import { useContext, useRef, useState } from "react";
+import { motion, useAnimate, stagger } from "framer-motion";
 
-import { ChallengesContext } from '../store/challenges-context.jsx';
-import Modal from './Modal.jsx';
-import images from '../assets/images.js';
+import { ChallengesContext } from "../store/challenges-context.jsx";
+import Modal from "./Modal.jsx";
+import images from "../assets/images.js";
 
 export default function NewChallenge({ onDone }) {
   const title = useRef();
   const description = useRef();
   const deadline = useRef();
+  const [scope, animate] = useAnimate(); //use to carryout animation imperatively. scope is used as ref, animate as function use to define the animation settings.
 
   const [selectedImage, setSelectedImage] = useState(null);
   const { addChallenge } = useContext(ChallengesContext);
@@ -31,6 +33,11 @@ export default function NewChallenge({ onDone }) {
       !challenge.deadline.trim() ||
       !challenge.image
     ) {
+      animate(
+        "input, textArea",
+        { x: [-10, 0, 10, 0] },
+        { type: "keyframes", duration: 0.5, delay: stagger(0.5) }
+      );
       return;
     }
 
@@ -40,7 +47,7 @@ export default function NewChallenge({ onDone }) {
 
   return (
     <Modal title="New Challenge" onClose={onDone}>
-      <form id="new-challenge" onSubmit={handleSubmit}>
+      <form id="new-challenge" onSubmit={handleSubmit} ref={scope}>
         <p>
           <label htmlFor="title">Title</label>
           <input ref={title} type="text" name="title" id="title" />
@@ -56,17 +63,31 @@ export default function NewChallenge({ onDone }) {
           <input ref={deadline} type="date" name="deadline" id="deadline" />
         </p>
 
-        <ul id="new-challenge-images">
+        <motion.ul
+          id="new-challenge-images"
+          variants={{
+            hiddenAnimation: {},
+            visibleAnimation: { transition: { staggerChildren: 0.5 } },
+          }}
+        >
           {images.map((image) => (
-            <li
+            <motion.li
+              variants={{
+                hiddenAnimation: { opacity: 0, scale: 0.5 },
+                visibleAnimation: { opacity: 1, scale: 1 },
+              }}
+              initial="hiddenAnimation"
+              animate="visibleAnimation"
+              exit={{ opacity: 1, scale: 1 }}
+              transition={{ type: "spring" }}
               key={image.alt}
               onClick={() => handleSelectImage(image)}
-              className={selectedImage === image ? 'selected' : undefined}
+              className={selectedImage === image ? "selected" : undefined}
             >
               <img {...image} />
-            </li>
+            </motion.li>
           ))}
-        </ul>
+        </motion.ul>
 
         <p className="new-challenge-actions">
           <button type="button" onClick={onDone}>
@@ -78,3 +99,8 @@ export default function NewChallenge({ onDone }) {
     </Modal>
   );
 }
+
+//variants enables us to activate the animate, exit, initial attribute in parent element on the child element if the properities of
+//the parent and child elements is the same though their value may vary.
+//e.g " initial={"hiddenAnimation"} animate={"visibleAnimation"} exit={"hiddenAnimation"}" defined in the parent component
+// are triggered for a child element that has a variant with the same attribiute as the parent.
