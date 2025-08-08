ae82d0f922c1d426b8b098a70f55fd93021192ba
David Akah <eliasakah1999@gmail.com>
2025-08-09 00:57:57 +0530

    work on pagination

--- Backend_Tutorial/NodeJs_Fundamentals/module-24-Rest_API/app.js
+++ Backend_Tutorial/NodeJs_Fundamentals/module-24-Rest_API/app.js
@@ -1,22 +1,54 @@
 const express = require("express");
+const path = require("path");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const multer = require("multer");
+const { v4: uuidv4 } = require("uuid");
 
 const feedRoute = require("./routes/feed");
 
 const app = express();
 dotenv.config();
 
+//creating a storage path for the uploaded file on your local folder
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, uuidv4() + path.extname(file.originalname));
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  console.log(file);
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
+);
 app.use(express.json()); //ensures json files are automatically parsed
 app.use(express.urlencoded({ extended: true })); //ensures that
 
 //creating a static file for every data request that starts from images
-app.use('/images', express.static('images'))
+app.use("/images", express.static("images"));
 
 //setting permission rules for which browsers is allowed to talk to your backend server
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Method", "GET, POST, PUT, PATCH, DELETE");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
   next();
@@ -42,6 +74,16 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+
+
+
+
+
+
+
+
+  
 
 // app.use((req, res, next) => {
 //   res.setHeader("Access-Control-Allow-Origin", "*"); //allow any website/ domain
--- Backend_Tutorial/NodeJs_Fundamentals/module-24-Rest_API/controllers/feed.js
+++ Backend_Tutorial/NodeJs_Fundamentals/module-24-Rest_API/controllers/feed.js
@@ -1,8 +1,37 @@
+const path = require("path");
+const fs = require("fs");
+const mongoose = require("mongoose");
+
 const { validationResult } = require("express-validator");
-const path = require(path);
+
 const Post = require("../model/posts");
 
 exports.getPosts = (req, res, next) => {
+  const currentPage = req.query.page || 1;
+  const postsPerPage = 2;
+  let totalPosts;
+  Post.find()
+    .countDocuments()
+    .then((count) => {
+      totalPosts = count;
+      return Post.find()
+        .skip((currentPage - 1) * postsPerPage)
+        .limit(postsPerPage)
+        .then((posts) => {
+          console.log(posts);
+          res.status(200).json({
+            posts: posts,
+            message: "Successfully fetched posts",
+            totalItems: totalPosts,
+          });
+        });
+    })
+    .catch((error) => {
+      if (!error.statusCode) {
+        error.statusCode = 500;
+      }
+      next(error);
+    });
   Post.find()
     .then((posts) => {
       console.log(posts);
@@ -21,12 +50,29 @@ exports.getPosts = (req, res, next) => {
 exports.createPost = (req, res, next) => {
   //we are expected to collect the data that was entered inside the form and use it to create a new post object
 
-  //replacing backslashes with forward slashes on the received image url
-  const ImageUrl = req.file.path.replace(/\\/g, "/");
+  const errors = validationResult(req);
+  //if errors object is not empty it means an error occured
+  if (!errors.isEmpty()) {
+    const error = new Error("Validation failed, entered data is invalid");
+    error.statusCode = 422;
 
+    throw error;
+  }
+
+  console.log("req.file:", req.file);
+  console.log("req.body:", req.body);
+
+  if (!req.file) {
+    const error = new Error("No image provided");
+    error.statusCode = 422;
+    throw error;
+  }
+
+  //replacing backslashes with forward slashes on the received imageUrl and pass the returned path to the varibale ImageUrl
+  const ImageUrl = req.file.path.replace(/\\/g, "/");
+  console.log("ImageUrl path:", ImageUrl);
   const title = req.body.title;
   const content = req.body.content;
-  const errors = validationResult(req);
 
   const post = new Post({
     title: title,
@@ -36,14 +82,6 @@ exports.createPost = (req, res, next) => {
       name: "David",
     },
   });
-
-  //if errors object is not empty it means an error occured
-  if (!errors.isEmpty()) {
-    const error = new Error("Validation failed, entered data is invalid");
-    error.statusCode = 422;
-
-    throw error;
-  }
 
   post
     .save()
@@ -81,4 +119,107 @@ exports.getPost = (req, res, next) => {
       }
       next(error);
     });
+};
+
+exports.updatePost = (req, res, next) => {
+  const postId = req.params.postId;
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    const error = new Error("Validation failed, entered data is invalid");
+    error.statusCode = 422;
+
+    throw error;
+  }
+
+  const title = req.body.title;
+  const content = req.body.content;
+
+  //if we don't upload a file during form edit ImageUrl is given as below
+  let ImageUrl = req.body.image;
+
+  //if file is uploaded during form edit ImageUrl is given as below
+  if (req.file) {
+    ImageUrl = req.file.path.replace(/\\/g, "/");
+  }
+
+  if (!ImageUrl) {
+    const error = new Error("No image provided");
+    error.statusCode = 422;
+    throw error;
+  }
+
+  Post.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const error = new Error("Could not find post");
+        error.statusCode = 404;
+        throw error;
+      }
+      console.log("ImageUrl path: ", ImageUrl);
+      console.log("post.ImageUrl Path: ", post.ImageUrl);
+      //comparing the new image path entered by the user and the previous image path from the database and
+      if (ImageUrl !== post.ImageUrl) {
+        clearImage(post.ImageUrl);
+      }
+      post.title = title;
+      post.content = content;
+      post.ImageUrl = ImageUrl;
+
+      return post.save(); //save the updated post to our mongoDB database
+    })
+    .then((result) => {
+      res.status(200).json({ message: "Updated post", post: result });
+    })
+    .catch((error) => {
+      if (!error.statusCode) {
+        error.statusCode = 500;
+      }
+      next(error);
+    });
+};
+
+exports.deletePost = (req, res, next) => {
+  const postId = req.params.postId;
+  console.log("postId: ", postId);
+
+  Post.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const error = new Error("Could not find post");
+        error.statusCode = 404;
+        throw error;
+      }
+      clearImage(post.ImageUrl);
+
+      console.log(
+        "if postId is valid: ",
+        mongoose.Types.ObjectId.isValid(postId)
+      );
+
+      return Post.findByIdAndRemove(postId);
+    })
+    .then((result) => {
+      res.status(200).json({ message: "Deleted post" });
+    })
+    .catch((error) => {
+      if (!error.statusCode) {
+        error.statusCode = 500;
+      }
+      next(error);
+    });
+};
+
+const clearImage = (filePath) => {
+  filePath = path.join(__dirname, "..", filePath);
+  console.log("filepath from clearImage: ", filePath);
+
+  //deletes a file from the images folder in your local folder
+  fs.unlink(filePath, (err) => {
+    if (err) {
+      console.error("Failed to delete file:", filePath, err);
+    } else {
+      console.log("Deleted file:", filePath);
+    }
+  });
 };
